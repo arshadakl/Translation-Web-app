6511f9a8a749ec6a7d8d61096b41a1d24d7b4f89
arshadakl <arshadayanikkal@gmail.com>
2023-11-15 21:56:34 +0530

    design added

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-$('#button').click(function(){translate();});
 
-function translate(){
-  
-  var sourceText = $('textarea#sourceText').val();
-  var sourceLang = 'en';
-  var targetLang = 'ar';
-  console.log(sourceText);
-  
-  var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
-  //console.log(url);
-  
-  $.getJSON(url, function(data) {
-      $('textarea#resultText').val(data[0][0][0]);
-    });
 
+function Trans(){
+        // alert("called")
+        var sourceText = document.getElementById("sourceText").value
+        var sourceLang = 'en';
+        var targetLang = 'ar';
+        console.log(sourceText);
+        
+        var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+        //console.log(url);
+        
+        $.getJSON(url, function(data) {
+           document.getElementById("resultText").value = data[0][0][0]
+            
+          });
+      
+      
 }
