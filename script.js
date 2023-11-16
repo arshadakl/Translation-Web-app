18b18bada46e8ed7a430e81c9dc1af1968507859
arshadakl <arshadayanikkal@gmail.com>
2023-11-16 11:19:55 +0530

    design added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Trans(){
         // alert("called")
         var sourceText = document.getElementById("sourceText").value
         var sourceLang = 'en';
-        var targetLang = 'ar';
+        var targetLang = 'ml';
         console.log(sourceText);
         
         var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
