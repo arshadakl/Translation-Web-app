4529d24ca68b1f71334c3c8722c55b34db69eee6
arshadakl <arshadayanikkal@gmail.com>
2023-11-20 21:53:59 +0530

    add new language

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Trans(){
         // alert("called")
         var sourceText = document.getElementById("sourceText").value
         var sourceLang = 'en';
-        var targetLang = 'ml';
+        var targetLang = 'hi';
         console.log(sourceText);
         
         var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
